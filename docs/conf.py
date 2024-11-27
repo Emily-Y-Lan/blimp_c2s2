aa68803894b21fcbc0ecbf5367a9598b0efd2b21
Aidan <acm289@cornell.edu>
2024-11-26 23:35:47 -0500

    Updated docs stuff

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,25 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Blimp'
-copyright = '2024, Aidan McNay'
-author = 'Aidan McNay'
+project = "Blimp"
+copyright = "2024, Aidan McNay"
+author = "Aidan McNay"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx_rtd_theme"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_logo = "img/blimp_no_border.png"
+html_css_files = [
+    "css/logo.css",
+]
